f35aaef2f9ec59e06bc759e534934f5b9b8325a3
Jorge Aceves <jorge_aceves_martin@outlook.com>
2021-12-05 21:15:03 -0800

    Day 6, Ruby

---
+++ day6/part_1_solution.rb
@@ -0,0 +1,26 @@
+def read_file(file_name)
+  file = File.open(file_name, "r")
+  data = file.read
+  file.close
+  return data
+end
+
+
+file_name = "input.txt"
+arr = read_file(file_name).split(",").map(&:to_i)
+total = arr.size
+days = {}
+for i in 0..8 do days[i] = 0 end
+arr.each {|e| days[e] += 1}
+for i in 1..80 do
+  new_days = {}
+  for k in 0..8 do new_days[k] = 0 end
+  total += days[0]
+  new_days[6] = days[0]
+  new_days[8] = days[0]
+  for i in 1..8 do
+    new_days[i-1] += days[i]
+  end
+  days = new_days
+end
+p total
